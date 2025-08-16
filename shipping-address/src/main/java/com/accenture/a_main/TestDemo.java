4914d71bc35005c71d230e40dec610e471a638bf
CapGeminiRepo150825 <vykuntarao.e@example.com>
2025-08-16 11:24:41 +0530

    testDemo file added

---
+++ shipping-address/src/main/java/com/accenture/a_main/TestDemo.java
@@ -0,0 +1,11 @@
+package com.accenture.a_main;
+
+import org.hibernate.internal.build.AllowSysOut;
+
+public class TestDemo {
+	
+	public void m1() {
+		System.out.println(" Tesd Dmeocalll");
+	}
+
+}
