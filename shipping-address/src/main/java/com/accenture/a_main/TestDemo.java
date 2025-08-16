9a29270414b7f31bee2a7748147c307640b5293d
VykuntaraoGitHub <144585061+VykuntaraoGitHub@users.noreply.github.com>
2025-08-16 11:32:36 +0530

    Update TestDemo.java

--- shipping-address/src/main/java/com/accenture/a_main/TestDemo.java
+++ shipping-address/src/main/java/com/accenture/a_main/TestDemo.java
@@ -5,7 +5,7 @@ import org.hibernate.internal.build.AllowSysOut;
 public class TestDemo {
 	
 	public void m1() {
-		System.out.println(" Tesd Dmeocalll");
+		System.out.println(" Tesd Dmeocalll - modifed for PULL command praactice");
 	}
 
 }
